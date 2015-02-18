92f1303f6326553047fbfc1d4128389d48b289ab
Nate Nichols <nathann@ori.org>
2015-02-17 20:52:15 -0800

    animations, correctness, results, scores, TODO:blocks

--- VisualSearch/visualsearch.js
+++ VisualSearch/visualsearch.js
@@ -1,9 +1,15 @@
 var stim_average = ['Stimuli/eAverage1.jpg', 'Stimuli/eAverage2.jpg', 'Stimuli/eAverage3.jpg', 'Stimuli/eAverage4.jpg', 'Stimuli/eAverage5.jpg', 'Stimuli/eAverage6.jpg', 'Stimuli/eAverage7.jpg', 'Stimuli/eAverage8.jpg', 'Stimuli/eAverage9.jpg', 'Stimuli/eAverage10.jpg', 'Stimuli/eAverage11.jpg', 'Stimuli/eAverage12.jpg', 'Stimuli/eAverage13.jpg', 'Stimuli/eAverage14.jpg', 'Stimuli/eAverage15.jpg', 'Stimuli/eAverage16.jpg', 'Stimuli/eAverage17.jpg', 'Stimuli/eAverage18.jpg', 'Stimuli/eAverage19.jpg', 'Stimuli/eAverage20.jpg', 'Stimuli/eAverage21.jpg', 'Stimuli/eAverage22.jpg', 'Stimuli/eAverage23.jpg', 'Stimuli/eAverage24.jpg', 'Stimuli/eAverage25.jpg', 'Stimuli/eAverage26.jpg', 'Stimuli/eAverage27.jpeg', 'Stimuli/eAverage28.jpg', 'Stimuli/eAverage29.jpg', 'Stimuli/eAverage30.jpg', 'Stimuli/eAverage31.jpg', 'Stimuli/eAverage32.jpg', 'Stimuli/eAverage33.jpg', 'Stimuli/eAverage34.jpg', 'Stimuli/eAverage35.jpg', 'Stimuli/eAverage36.jpg', 'Stimuli/eAverage37.jpg', 'Stimuli/eAverage38.jpg', 'Stimuli/eAverage39.jpg', 'Stimuli/eAverage40.jpg', 'Stimuli/eAverage41.jpg', 'Stimuli/eAverage42.jpg', 'Stimuli/eAverage43.jpg', 'Stimuli/eAverage44.jpg', 'Stimuli/eAverage45.jpg', 'Stimuli/eAverage46.jpg', 'Stimuli/eAverage47.jpg', 'Stimuli/eAverage48.jpg', 'Stimuli/eAverage49.jpg', 'Stimuli/eAverage50.jpg', 'Stimuli/eAverage51.jpg', 'Stimuli/eAverage52.jpg', 'Stimuli/eAverage53.jpg', 'Stimuli/eAverage54.jpg', 'Stimuli/eAverage55.jpg', 'Stimuli/eAverage56.jpg', 'Stimuli/eAverage57.jpg', 'Stimuli/eAverage58.jpg', 'Stimuli/eAverage59.jpg', 'Stimuli/eAverage60.jpg', 'Stimuli/eAverage61.jpg', 'Stimuli/eAverage62.jpg', 'Stimuli/eAverage63.jpg', 'Stimuli/eAverage64.jpg', 'Stimuli/eAverage65.jpg', 'Stimuli/eAverage66.jpg', 'Stimuli/eAverage67.jpg', 'Stimuli/eAverage68.jpg', 'Stimuli/eAverage69.jpg', 'Stimuli/eAverage70.jpg', 'Stimuli/eAverage71.jpg', 'Stimuli/eAverage72.jpg', 'Stimuli/eAverage73.jpg', 'Stimuli/eAverage74.jpg', 'Stimuli/eAverage75.jpg', 'Stimuli/eAverage76.jpg', 'Stimuli/eAverage77.jpg', 'Stimuli/eAverage78.jpg', 'Stimuli/eAverage79.jpg', 'Stimuli/eAverage80.jpg'];
 var stim_ultra = ['Stimuli/eUltraT1.jpg', 'Stimuli/eUltraT2.jpg', 'Stimuli/eUltraT3.jpg', 'Stimuli/eUltraT4.jpg', 'Stimuli/eUltraT5.jpg', 'Stimuli/eUltraT6.jpg', 'Stimuli/eUltraT7.jpg', 'Stimuli/eUltraT8.jpg', 'Stimuli/eUltraT9.jpg', 'Stimuli/eUltraT10.jpg', 'Stimuli/eUltraT11.jpg', 'Stimuli/eUltraT12.jpg', 'Stimuli/eUltraT13.jpg', 'Stimuli/eUltraT14.jpg', 'Stimuli/eUltraT15.jpg', 'Stimuli/eUltraT16.jpg', 'Stimuli/eUltraT17.jpg', 'Stimuli/eUltraT18.jpg', 'Stimuli/eUltraT19.jpg', 'Stimuli/eUltraT20.jpg', 'Stimuli/eUltraT21.jpg', 'Stimuli/eUltraT22.jpg', 'Stimuli/eUltraT23.jpg', 'Stimuli/eUltraT24.jpg', 'Stimuli/eUltraT25.jpg', 'Stimuli/eUltraT26.jpg', 'Stimuli/eUltraT27.jpg', 'Stimuli/eUltraT28.jpg', 'Stimuli/eUltraT29.jpg', 'Stimuli/eUltraT30.JPG', 'Stimuli/eUltraT31.jpg', 'Stimuli/eUltraT32.jpg', 'Stimuli/eUltraT33.jpg', 'Stimuli/eUltraT34.jpg', 'Stimuli/eUltraT35.jpg', 'Stimuli/eUltraT36.jpg', 'Stimuli/eUltraT37.jpg', 'Stimuli/eUltraT38.jpg', 'Stimuli/eUltraT39.jpg', 'Stimuli/eUltraT40.jpg', 'Stimuli/eUltraT41.jpg', 'Stimuli/eUltraT42.jpg', 'Stimuli/eUltraT43.jpg', 'Stimuli/eUltraT44.jpeg', 'Stimuli/eUltraT45.jpg', 'Stimuli/eUltraT46.jpg', 'Stimuli/eUltraT47.jpg', 'Stimuli/eUltraT48.jpg', 'Stimuli/eUltraT49.jpg', 'Stimuli/eUltraT50.jpg', 'Stimuli/eUltraT51.jpg', 'Stimuli/eUltraT52.jpg', 'Stimuli/eUltraT53.jpg', 'Stimuli/eUltraT54.jpg', 'Stimuli/eUltraT55.jpg', 'Stimuli/eUltraT56.jpg', 'Stimuli/eUltraT57.jpg', 'Stimuli/eUltraT58.jpg', 'Stimuli/eUltraT59.jpg', 'Stimuli/eUltraT60.jpg', 'Stimuli/eUltraT61.jpg', 'Stimuli/eUltraT62.jpg', 'Stimuli/eUltraT63.jpg', 'Stimuli/eUltraT64.jpg', 'Stimuli/eUltraT65.jpg', 'Stimuli/eUltraT66.jpg', 'Stimuli/eUltraT67.jpg', 'Stimuli/eUltraT68.jpg', 'Stimuli/eUltraT69.jpg', 'Stimuli/eUltraT70.jpg', 'Stimuli/eUltraT71.jpg', 'Stimuli/eUltraT72.jpg', 'Stimuli/eUltraT73.jpg', 'Stimuli/eUltraT74.jpg', 'Stimuli/eUltraT75.jpg', 'Stimuli/eUltraT76.jpg', 'Stimuli/eUltraT77.jpg', 'Stimuli/eUltraT78.jpg', 'Stimuli/eUltraT79.jpg', 'Stimuli/eUltraT80.jpg'];
-var results = [];
+var RESULTS = [];
+var ALL_RESULTS = [];
 
 var MATRIX_X_SIZE = 2;
 var MATRIX_Y_SIZE = 5;
+var TRIALS_PER_BLOCK = 10;
+var BLOCK_NUM = 2;
+
+var TRIAL_COUNT = 0;
+var BLOCK_COUNT = 0;
 
 function getCoord(){
   var x = Math.floor(Math.random()*10)%MATRIX_X_SIZE;
@@ -14,6 +20,14 @@ function getCoord(){
           x:x,
           y:y
         }
+}
+
+function getScore(){
+
+  return {
+    simple : _.filter(RESULTS,function(r){return r.correct;}).length+'/'+RESULTS.length,
+    percent : Math.floor(100*_.filter(RESULTS,function(r){return r.correct;}).length/RESULTS.length)+'%'
+  }
 }
 
 function buildImageMatrix(incorrect, correct){
@@ -45,35 +59,79 @@ function matrix_to_html(matrix){
   html+="</table>";
   return html;
 }
+var now = Date.now();
 
+$('body').html('<h1>Instructions</h1><p>Click the average sized model. Click <a href="#" id="skippy">here</a> to continue.</p>');
 
-setInterval(function() {
-  var now = Date.now();
-  var no_click = true;
-
-  $('body').html(matrix_to_html(buildImageMatrix(stim_ultra,stim_average)));
-
-  $('img').on('click', function(){
-
-    $el = $(this);
-    if(no_click){
-      if(String($el.attr('src')).slice(9,10)=='A'){
-        $el.parent().addClass('correct');
-        results.push({rt:Math.abs(now-Date.now()), correct:true})
-      } else {
-        // find the good one, add green class
-        $el.parent().addClass('incorrect');
-        results.push({rt:Math.abs(now-Date.now()), correct:false})
-      }  
-    }
-    no_click = false;
+function skipOnKey(){
+  $('#skippy').on('click', function(){
+    $('body').html(matrix_to_html(buildImageMatrix(stim_ultra,stim_average)));
+    now = Date.now();
+    nextTrialArray();
   });
+}
+skipOnKey();
+function nextTrialArray(){ 
+  $('img').on('click', function(){
+    $el = $(this);
+    if(String($el.attr('src')).slice(9,10)=='A'){
+      $el.parent().addClass('correct');
+      $el.addClass('animated tada');
+      
+      // $incorrect_el = $(_.filter($('img'), function(img){return $(img).attr('src').slice(9,10)=='U';}));
+      // $incorrect_el.parent().addClass('correct');
+      // $incorrect_el.addClass('animated zoomOut');
 
-  if(no_click){
-    results.push({rt:-1,correct:false})
-  }
+      RESULTS.push({rt:Math.abs(now-Date.now()), correct:true})
+    } else {
 
-  
+      $el.parent().addClass('incorrect');
+      $el.addClass('animated shake');
+      RESULTS.push({rt:Math.abs(now-Date.now()), correct:false})
 
-}, 3000);
+      $correct_el = $(_.find($('img'), function(img){return $(img).attr('src').slice(9,10)=='A';}));
+      $correct_el.parent().addClass('correct');
+      $correct_el.addClass('animated wobble');
+
+    }
+
+    setTimeout(function(){
+      // Hide everything 2 seconds after the click
+      $('body').html('');
+      TRIAL_COUNT++;
+    }, 2000);
+
+    setTimeout(function(){
+      // Show everything 500ms after hiding
+      if(TRIAL_COUNT==TRIALS_PER_BLOCK){
+        TRIAL_COUNT=0;
+        if(BLOCK_COUNT==BLOCK_NUM){
+          // end trials
+          $('body').html("<h1>Great job! That's all there is.</h1>");
+          console.log(ALL_RESULTS);
+
+        }
+        BLOCK_COUNT++;
+        
+        $('body').html(function(){return "<h1 id='skippy'>So far so good...</h1><p><ul><li>Percentage Correct:"+getScore().percent+"</li><li>Score: "+getScore().simple+"</li></ul>Click <a href='#' id='skippy'>here</a> to continue.</p>";});
+        skipOnKey();
+        now = Date.now();
+        nextTrialArray();
+        ALL_RESULTS.push(RESULTS); // save last trials results
+        RESULTS = []; // clear RESULTS
+      }else{
+        $('body').html(matrix_to_html(buildImageMatrix(stim_ultra,stim_average)));
+        now = Date.now();
+        nextTrialArray();
+      }
+    }, 2500);
+  });
+}
+
+// TODO: Ask questions:
+//    - What does "preceded by a 500ms fixation cross" mean?
+// TODO: welcome, debreif blocks, inter-experiment RESULTS.
+// TODO: 1000ms pause to show/animate correct/incorrect responses
+// TODO: finite sets of trials (4 blocks, 30 arrays per block)
+// TODO: add and remove event listeners
 
